8111a683f423938dbefedfb2ca90129ba061cfb6
Dayasharma4125 <108992416+Dayasharma4125@users.noreply.github.com>
2023-07-13 06:29:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express=require("express")
 const connectdb=require('./component/database.js')
-
+const Port=process.env.Port||6020
 const app=express()
 connectdb();
 app.use(express.json())
 // console.log(process.env)
-app.listen(5001,()=>{console.log('server is running at 5001')})
+app.listen(Port,()=>{console.log(`server is running at ${Port}`)})
 
 app.use('/',require('./container/container.js'))
 app.use('/cart',require('./component/cart.js'))
